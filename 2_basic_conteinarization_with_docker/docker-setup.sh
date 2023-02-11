bbdd3d37887f54cf2c4a9417afef1d623fa49d6c
Petar <petarivanovgap@gmail.com>
2023-02-11 22:07:36 +0200

    Homework is done for basic containerization part

---
+++ 2_basic_conteinarization_with_docker/docker-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
+
+echo "* Update base image ..."
+dnf upgrade
+
+echo "* Add hosts ..."
+echo "192.168.99.100 docker.do1.lab docker" >> /etc/hosts
+
+echo "* Add Docker repository ..."
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+echo "* Install Docker ..."
+dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+echo "* Enable and start Docker ..."
+systemctl enable docker
+systemctl start docker
+
+echo "* Firewall - open port 8080 ..."
+firewall-cmd --add-port=8080/tcp --permanent
+firewall-cmd --reload
+
+echo "* Add vagrant user to docker group ..."
+usermod -aG docker vagrant
