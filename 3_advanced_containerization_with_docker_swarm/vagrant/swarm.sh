70674d2dbc28c130b25b0373ff51e3c790bb89bf
Petar <petarivanovgap@gmail.com>
2023-02-21 23:25:05 +0200

    changed swarm script

--- 3_advanced_containerization_with_docker_swarm/vagrant/swarm.sh
+++ 3_advanced_containerization_with_docker_swarm/vagrant/swarm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-LEADER_IP='192.168.99.101'
-
-sshpass -p 'vagrant' ssh vagrant@docker1 docker swarm init --advertise-addr $LEADER_IP
-
-WORKER_TOKEN=$(sshpass -p 'vagrant' ssh vagrant@docker1 docker swarm join-token -q worker) 
+LEADER_IP='192.168.99.103'
+echo LEADER_IP >> pepe.txt
+docker swarm init --advertise-addr $LEADER_IP
+echo 'swarm init' >> pepe.txt
+WORKER_TOKEN= docker swarm join-token -q worker 
 echo $WORKER_TOKEN >> pepe.txt
 sshpass -p 'vagrant' ssh vagrant@docker2 docker swarm join --token $WORKER_TOKEN --advertise-addr 192.168.99.102 $LEADER_IP:2377
-sshpass -p 'vagrant' ssh vagrant@docker3 docker swarm join --token $WORKER_TOKEN --advertise-addr 192.168.99.103 $LEADER_IP:2377
+sshpass -p 'vagrant' ssh vagrant@docker1 docker swarm join --token $WORKER_TOKEN --advertise-addr 192.168.99.101 $LEADER_IP:2377
+echo 'Done' >> pepe.txt
+docker stack deploy -c docker-compose.yml bgapp
