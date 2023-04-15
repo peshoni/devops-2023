194770db1b53a106700272900bcbbed0e32f9b94
Petar <petarivanovgap@gmail.com>
2023-04-15 23:27:05 +0300

    M2-2 Ansible playbooks

--- 2_2_ansible/install_ansible_centos.sh
+++ 2_2_ansible/install_ansible_centos.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 dnf install -y ansible-core
+dnf install -y python-setuptools
+
 ansible-galaxy collection install -p /usr/share/ansible/collections ansible.posix
+ansible-galaxy collection install community.docker
+ansible-galaxy collection install community.mysql
